575c3ee8ee4b2537fbe3b981aa146a3243dda793
Sahil Rajput <rajputsahil.2204@gmail.com>
2020-04-09 01:10:26 +0530

    Database File Added

---
+++ get_your_designer.sql
@@ -0,0 +1,250 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 08, 2020 at 09:37 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `get_your_designer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking_design`
+--
+
+CREATE TABLE `booking_design` (
+  `id` int(5) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `design_id` varchar(10) NOT NULL,
+  `design_name` varchar(20) NOT NULL,
+  `price` varchar(20) NOT NULL,
+  `designer_email` varchar(50) NOT NULL,
+  `size` varchar(10) NOT NULL,
+  `booking_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(5) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact` varchar(500) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designer_post`
+--
+
+CREATE TABLE `designer_post` (
+  `id` int(3) NOT NULL,
+  `designer_email` varchar(50) NOT NULL,
+  `design_name` varchar(50) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `material` varchar(50) NOT NULL,
+  `price` bigint(10) NOT NULL,
+  `size` varchar(50) NOT NULL,
+  `category` varchar(20) NOT NULL,
+  `occasion` varchar(20) NOT NULL,
+  `images` varchar(200) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designer_profile`
+--
+
+CREATE TABLE `designer_profile` (
+  `id` int(3) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `profile_img` varchar(200) NOT NULL,
+  `street` varchar(50) NOT NULL,
+  `area` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `pin_code` varchar(50) NOT NULL,
+  `website` varchar(300) NOT NULL,
+  `instagram` varchar(300) NOT NULL,
+  `facebook` varchar(300) NOT NULL,
+  `twitter` varchar(300) NOT NULL,
+  `blogger` varchar(300) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(5) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `feedback` varchar(500) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register_designer`
+--
+
+CREATE TABLE `register_designer` (
+  `id` int(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `c_password` varchar(20) NOT NULL,
+  `profile_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_register`
+--
+
+CREATE TABLE `user_register` (
+  `id` bigint(5) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `c_password` varchar(50) NOT NULL,
+  `v_flag` varchar(1) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking_design`
+--
+ALTER TABLE `booking_design`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `designer_post`
+--
+ALTER TABLE `designer_post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `designer_profile`
+--
+ALTER TABLE `designer_profile`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `register_designer`
+--
+ALTER TABLE `register_designer`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `user_register`
+--
+ALTER TABLE `user_register`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking_design`
+--
+ALTER TABLE `booking_design`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `designer_post`
+--
+ALTER TABLE `designer_post`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `designer_profile`
+--
+ALTER TABLE `designer_profile`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `register_designer`
+--
+ALTER TABLE `register_designer`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_register`
+--
+ALTER TABLE `user_register`
+  MODIFY `id` bigint(5) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
